dc02774e2a7ca18831bf480e4cafddb80e2880db
Jalen Stephens <108702328+Jalen-Stephens@users.noreply.github.com>
2025-04-21 16:58:56 -0400

    Added lessons data

--- server.py
+++ server.py
@@ -23,6 +23,34 @@ questions = {
     }
 }
 
+lessons = {
+    0:{
+        "lesson_id": "0",
+        "Section": "Posistions", 
+        "title": "Neutral",
+        "main-text": "Every match starts in “Neutral”, where both wrestlers are on their feet facing each other.",
+        "sub-text": ['Crouched down in their “stances”','Looking for offensive and defensive scoring'],
+        "media": "https://images2.minutemediacdn.com/image/upload/c_crop,w_4010,h_2255,x_0,y_0/c_fill,w_2160,ar_16:9,f_auto,q_auto,g_auto/images%2FImagnImages%2Fmmsport%2Fall_penn_state%2F01jpxxdds2hs0ce4jydd.jpg",
+    },
+    1:{
+        "lesson_id": "1",
+        "Section": "Posistions", 
+        "title": "Top",
+        "main-text": '“Top” is when one wrestler is on top of the other (Black Singlet)',
+        "sub-text": ['May be chosen as starting position for 2nd and 3rd periods','Results after one wrestler takes the other from neutral to the mat'],
+        "media": "https://www.wrestlingmindset.com/wp-content/uploads/2018/01/Top-Position.jpg",
+    },
+    2:{
+        "lesson_id": "2",
+        "Section": "Posistions", 
+        "title": "Bottom",
+        "main-text": '“Bottom” is when one wrestler is under the other (White Singlet)',
+        "sub-text": ['May be chosen as starting position for 2nd and 3rd periods','Results after one wrestler takes the other from neutral to the mat'],
+        "media": "https://www.wrestlingmindset.com/wp-content/uploads/2018/01/Top-Position.jpg",
+    }
+}
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
